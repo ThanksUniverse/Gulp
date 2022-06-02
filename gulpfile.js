93d5031752d6a82ab204737a973cb0252b065f73
Pedro <thanksuniverse333@outlook.com>
2022-06-02 15:35:08 -0300

    Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require("gulp");
+const { parallel  } = require("gulp");
 const concat = require("gulp-concat");
 const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 const images = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
+const babel = require('gulp-babel')
 
 function tarefasCSS(cb) {
-	return gulp
-		.src([
+	 gulp.src([
 			"./node_modules/bootstrap/dist/css/bootstrap.css",
 			"./node_modules/@fortawesome/fontawesome-free/css/all.css",
 			"./vendor/owl/css/owl.css",
@@ -18,28 +20,35 @@ function tarefasCSS(cb) {
 		.pipe(cssmin())
 		.pipe(rename({ suffix: ".min" })) //libs.min.css
 		.pipe(gulp.dest("./dist/css"));
+
+		cb()
 }
 
-function tarefasJS() {
-	return gulp
-		.src([
-		"./node_modules/jquery/dist/jquery.js",
-		"./node_modules/bootstrap/dist/js/bootstrap.js",
-		"./node_modules/@fortawesome/fontawesome-free/js/all.js",
-		"./vendor/owl/js/owl.js",
-		"./vendor/jquery-ui/jquery-ui.js",
-		"./vendor/jquery-mask/jquery.mask.js",
-		"./assets/js/custom.js",
+function tarefasJS(callback) {
+	gulp.src([
+	"./node_modules/jquery/dist/jquery.js",
+	 "./node_modules/bootstrap/dist/js/bootstrap.js",
+	  "./node_modules/@fortawesome/fontawesome-free/js/all.js",
+	   "./vendor/owl/js/owl.js",
+		 "./vendor/jquery-ui/jquery-ui.js",
+		 "./vendor/jquery-mask/jquery.mask.js",
+		  "./assets/js/custom.js"
 		])
+		.pipe(babel({
+			comments: false,
+			presets: ['@babel/env'],
+			comapct: true
+		}))
 		.pipe(concat("scripts.js"))
 		.pipe(uglify())
 		.pipe(rename({ suffix: ".min" })) //libs.min.js
 		.pipe(gulp.dest("./dist/js"));
+
+		return callback();
 }
 
-function tarefasIMG() {
-	return gulp
-		.src("./assets/images/*")
+function tarefasIMG(callback) {
+	 return gulp.src("./assets/images/*")
 		.pipe(
 			images({
 				pngquant: true,
@@ -55,10 +64,23 @@ function tarefasIMG() {
 		)
 		.pipe(gulp.dest("./dist/images"));
 }
+//POC -- Proof of Concept
+function tarefasHTML(callback) {
+	gulp.src('assets/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+
+
+	 return callback()
+};
+
 
 exports.styles = tarefasCSS;
 exports.scripts = tarefasJS;
+exports.html = tarefasHTML;
 exports.images = tarefasIMG;
+
+exports.default = parallel( tarefasHTML, tarefasJS, tarefasCSS );
 
 //Gulp-concat Unifica os arquivos
 //Gulp-cssmin Minifica o CSS
